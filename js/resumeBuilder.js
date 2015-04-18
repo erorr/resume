966eac6cc5b105d0d1529423f39f274d89fc2e79
erorr <serchim@gmail.com>
2015-04-18 11:33:34 +0800

    Work Experience - Completed
    :last is removed without issue

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -45,6 +45,46 @@ if (bio.skills.length > 0) {
 	}
 }
 
+var work = {
+    "jobs": [
+        {
+            "employer": "Typo Incorporated",
+            "title": "Web Developer",
+            "location": "Singapore",
+            "dates": "2003 - 2009",
+            "description": "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar."
+        },
+        {
+            "employer": "Erorr Incorporated",
+            "title": "Website Administrator",
+            "location": "Thailand",
+            "dates": "2009 - Present",
+            "description": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?"
+        }
+    ]
+}
+
+if (work.jobs.length > 0) {
+	$("#workExperience").append(HTMLworkStart);
+
+	for (var job in work.jobs) {
+		
+		var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+    	var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+    	var formattedEmployerTitle = formattedEmployer + formattedTitle;
+    	$(".work-entry").append(formattedEmployerTitle);
+
+    	var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+    	$(".work-entry").append(formattedDates);
+
+    		var formattedWorkLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+    		$(".work-entry").append(formattedWorkLocation);
+    		
+    		var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+    		$(".work-entry").append(formattedDescription);
+	}
+}
+
 var education = {
     "schools": [
         {
@@ -84,24 +124,6 @@ var education = {
     ]
 }
 
-var work = {
-    "jobs": [
-        {
-            "employer": "Typo Incorporated",
-            "title": "Web Developer",
-            "location": "Singapore",
-            "dates": "2003 - 2009",
-            "description": "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar."
-        },
-        {
-            "employer": "Erorr Incorporated",
-            "title": "Website Administrator",
-            "location": "Thailand",
-            "dates": "2009 - Present",
-            "description": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?"
-        }
-    ]
-}
 
 var projects = {
     "projects": [
